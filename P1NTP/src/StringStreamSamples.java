5bd2d3798d246c10e9dba87f9505d80102023bab
Alejandro Alcalde <algui91@gmail.com>
2016-03-04 10:31:14 +0100

    Added files

--- P1NTP/src/StringStreamSamples.java
+++ P1NTP/src/StringStreamSamples.java
@@ -14,8 +14,37 @@
  * limitations under the License.
  */
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 /**
  * Created by Alejandro Alcalde <contacto@elbauldelprogramador.com> on 3/4/16.
  */
 public class StringStreamSamples {
+    public static void main(String[] args) {
+        String[] cadenas =
+                {"Rojo", "Naranja", "Amarillo", "Verde", "azul", "indigo", "Violeta"};
+
+        System.out.printf("Cadenas almacenadas: %s%n", Arrays.asList(cadenas));
+
+        // E1 - PAsar a mayus
+        System.out.printf("Cadenas en mayus: %s%n",
+                Arrays.stream(cadenas)
+                        .map(String::toUpperCase)
+                        .collect(Collectors.toList()));
+
+        // E2 - Las cadenas mayores que m, sin tener en cuenta mayus y minus se ordenan
+        System.out.printf("Cadenas filtradas y ordenadas: %s%n",
+                Arrays.stream(cadenas)
+                        .filter(s -> s.compareToIgnoreCase("m") > 0)
+                        .sorted(String.CASE_INSENSITIVE_ORDER)
+                        .collect(Collectors.toList()));
+
+        // E3 - Ordenas mayores que m descendentemente
+        System.out.printf("Cadenas filtradas y ordenadas descendentemente: %s%n",
+                Arrays.stream(cadenas)
+                        .filter(s -> s.compareToIgnoreCase("m") > 0)
+                        .sorted(String.CASE_INSENSITIVE_ORDER.reversed())
+                        .collect(Collectors.toList()));
+    }
 }
