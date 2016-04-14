4136ea5aeba9885e9274287db9a5e2b83532c8fd
Alejandro Alcalde <algui91@gmail.com>
2016-04-14 13:26:46 +0200

    Passed all pascal tests

--- RecursividadScala/src/Main.scala
+++ RecursividadScala/src/Main.scala
@@ -3,30 +3,21 @@
   * Objeto singleton para probar la funcionalidad del triangulo
   * de Pascal
   */
-object TrianguloPascal {
+object Main {
 
   def main(args: Array[String]) {
     // Se muestran 10 filas del trinagulo de Pascal
     for (row <- 0 to 10) {
       // Se muestran 10 y 10 filas
       for (col <- 0 to row)
-        print(calcularValorTrianguloPascal(col, row) + " ")
+        print(calcularValorTrianguloPascal(col, row) + "\t")
       // Salto de linea final para mejorar la presentacion
       println()
     }
     // Se muestra el valor que debe ocupar la columna 5 en la fila 10
     print(calcularValorTrianguloPascal(10, 15))
-
-
-
-//    prettytri(5)
   }
 
-//  def prettytri(n: Int) =
-//    (1 to n) foreach { i => print(" " * (n - i));
-//      calcularValorTrianguloPascal(i) map (c => print(c + " "));
-//      println
-//    } //> prettytri: (n: Int)Unit
   /**
     * Ejercicio 1: funcion para generar el triangulo de Pascal
     *
@@ -46,8 +37,23 @@ object TrianguloPascal {
     * @return valor booleano con el resultado de la operacion
     */
   def chequearBalance(cadena: List[Char]): Boolean = {
-    true
+    def contarParentesis(cadena: List[Char], contador: Int): Int = {
+
+      if (cadena.isEmpty) {
+        contador
+      } else {
+        val acumulador = cadena.head match {
+          case ')' => contador - 1 // Esto va a acumulador
+          case '(' => contador + 1
+          case _ => contador
+        }
+        if (acumulador < 0 ) acumulador
+        else contarParentesis(cadena.tail, acumulador)
+      }
+    }
+    contarParentesis(cadena, 0) == 0 // Como en R, ultima sentencia es el return
   }
+
 
   /**
     * Ejercicio 3: funcion para determinar las posibles formas de devolver el
